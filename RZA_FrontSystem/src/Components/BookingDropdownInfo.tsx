724360ba90b4f911bc4b3f442195d3778e35e80f
finley <144432889+finnerz5@users.noreply.github.com>
2024-04-18 14:00:34 +0000

    Working on creating form

--- RZA_FrontSystem/src/Components/BookingDropdownInfo.tsx
+++ RZA_FrontSystem/src/Components/BookingDropdownInfo.tsx
@@ -1,20 +1,26 @@
-import { MDBDropdown, MDBBtn, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdb-react-ui-kit";
+import "../CSS_pages/BookingStyle.css"
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-bootstrap";
 
 function BookingDrop (){
     // a drop down that provides users on info about scam ticket sellers
 
     return (
-        <MDBDropdown className='btn-group'>
-          <MDBBtn>Be weary of scam ticket sellers!</MDBBtn>
-          <MDBDropdownToggle split></MDBDropdownToggle>
-          <MDBDropdownMenu style={{ maxWidth: '500px' }}>
-          <p>We are well aware that people promote our tickets at a 'larger discounted price'.
-            At RZA we will only ever promote our discounts through our Official website and social media pages,
-            never on anything else.
-          </p>
-          </MDBDropdownMenu>
-        </MDBDropdown>
-      );
+      <Accordion>
+        <AccordionItem eventKey="1" className="w-25 mt-5 accordionCSS">
+          <AccordionHeader>Be weary of scam ticket sellers!</AccordionHeader>
+            <AccordionBody>
+              We are well aware that people promote our tickets at a 'larger discounted price'.
+              At RZA we will only ever promote our discounts through our Official website and social media pages,
+              never on anything else.
+            </AccordionBody>
+        </AccordionItem>
+      </Accordion>
+    );
 }
 
 export default BookingDrop;
+
+
+
+
+          
---
+++ RZA_FrontSystem/src/Components/BookingFormPages/Summary.tsx
@@ -0,0 +1,38 @@
+
+import FormWrapper from "../FormWrapper";
+import { FormData } from "../../types";
+
+const Summary = ({
+  firstName,
+  lastName,
+  emailAdress,
+  fullPriceTickets,
+  reducedPriceTickets,
+}: Partial<FormData>) => {
+  return (
+    <FormWrapper title="Summary">
+      <div className="leading-relaxed font-light text-gray-700 sm:mt-2 mt-12">
+        {firstName} {lastName}, your order consists of:
+        <br />
+        <div className="flex flex-col  gap-2 mx-4 my-4">
+          {fullPriceTickets && parseInt(fullPriceTickets) >= 1 && (
+            <span>
+              {fullPriceTickets} full price ticket
+              {parseInt(fullPriceTickets) > 1 ? "s" : ""}
+            </span>
+          )}
+          {reducedPriceTickets && parseInt(reducedPriceTickets) >= 1 && (
+            <span>
+              {reducedPriceTickets} reduced price ticket
+              {parseInt(reducedPriceTickets) > 1 ? "s" : ""}
+            </span>
+          )}
+        </div>
+        Confirm your order below and receive order summary at {emailAdress}.
+        <br />
+      </div>
+    </FormWrapper>
+  );
+};
+
+export default Summary;
